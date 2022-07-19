9774d886957f2ef344be4e1468f3d5f828996473
DanielJDufour <daniel.j.dufour@gmail.com>
2022-07-19 10:53:34 -0400

    adjusted wait time in test

--- test/test.wait.js
+++ test/test.wait.js
@@ -1,10 +1,10 @@
 const test = require("flug");
 const srvd = require("../srvd");
 
-test("shutting down after 5 seconds", ({ eq }) => {
+test("shutting down after 3 seconds", ({ eq }) => {
   srvd.serve({
     debug: true,
     port: 8082,
-    wait: 5
+    wait: 3
   });
 });
